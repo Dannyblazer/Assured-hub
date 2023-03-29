e4f2e61f47731a392352a4896afb0b40d71dcc79
Nnaji Daniel <43733933+Dannyblazer@users.noreply.github.com>
2023-03-29 08:14:21 +0000

    Main urls update

--- Blogz/urls.py
+++ Blogz/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.auth import views as auth_views
-from Blogz.settings import *
+import settings
 from personal.views import home_view, news_articles
 from django.conf.urls.static import static
 
